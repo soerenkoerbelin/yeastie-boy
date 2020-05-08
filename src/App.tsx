9e3a95a7e310fbde666a9de1ce538f3398b4021a
soeren.koerbelin <soeren.koerbelin@gmail.com>
2020-05-08 10:20:58 +0200

    basic stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
+const Home  = lazy(() => import('./js/components/Home/Home'))
+const Recipe  = lazy(() => import('./js/components/Recipe/Recipe'))
+const Experimental  = lazy(() => import('./js/components/Experimental/Experimental'))
+const About  = lazy(() => import('./js/components/About/About'))
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/recipe">Recipe</Link>
+          </li>
+          <li>
+            <Link to="/experimental">Experimental</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/recipe">
+          <Recipe />
+        </Route>
+        <Route path="/experimental">
+          <Experimental />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
